24099a23e0fdf0ea2f5363559a191afeaf1964be
Steven Nuno <27190701-stevenn102272@users.noreply.replit.com>
2024-09-24 16:40:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+a = 20
+b = 30
+count = 0
+if a < 0:
+  a = -a
+if b < 0:
+  b = -b 
+
+while b >0:
+  while b <= a:
+    a = a - b
+  c = a
+  a = b
+  b = c
+  
+  count += 1
+  print("Loop # ", count) 
+  print("a is", a)
+  print("b is", b)
+  print("c is", c)
+
+
+print()
+print("Final count of a is", a)
+
+num = int(input())
+if num > 0:
+  print(-num, "The number is negative")
+else:
+  print(num * -1, "The number is positive")
+
